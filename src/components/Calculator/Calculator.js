55cd0956510775f40ac45ffa804071e4a1de0722
Kuba Gałek <xwondunx@gmail.com>
2020-12-22 23:08:05 +0100

    select with fetching data

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button} from '../Button';
 
 //  state = {
@@ -11,30 +11,49 @@ import {Button} from '../Button';
 // })
 
 function Select({value,setCurrency}) {
+  const [currencies, setCurrencies] = useState([]); // 'AUD', 'BGN'...
+
+// runs each time Select is mounted in code
+  useEffect(() => {
+
+    fetch(`https://api.ratesapi.io/api/latest?base=PLN`)
+    .then(response => response.json())
+    .then (data => {
+      setCurrencies(Object.keys(data.rates))
+    });
+
+    return () => {
+      //clearInterval
+      //removeEventListener
+    }
+  }, []);
+
+
   return(
     <select value={value} onChange={(event) => setCurrency(event.target.value)}>
-        <option value="USD">USD</option>
-        <option value="PLN">PLN</option>
-        <option value="GBP">GBP</option>
-        <option value="EUR">EUR</option>
+      {currencies.map((elem,) => <option key={`curr-${elem}`} value={elem}>{elem}</option>)}
     </select>
   );
 }
 
 function Calculator(params) {
+  const [result, setResult] = useState(0);
   const [amount, setAmount] = useState(0);
-  const [currencyFrom, setCurrencyFrom] =useState('PLN');
-  const [currencyTo, setCurrencyTo] =useState('USD');
+  const [currencyFrom, setCurrencyFrom] = useState('PLN');
+  const [currencyTo, setCurrencyTo] = useState('USD');
   const handleSubmit = (event) => {
     event.preventDefault();
+    fetch(`https://api.ratesapi.io/api/latest?base=${currencyFrom}`)
+    .then(response => response.json())
+    .then (data => {
+      setResult(amount * data.rates[currencyTo]);
+    });
   }
   return(
         <form onSubmit={handleSubmit}>
         <div>
-          <input type="number" placeholder="Amount" onChange={
-            (event) => {
-              setAmount(event.target.value);
-            }} />
+          <input type="number" placeholder="Amount" onChange={(event) => setAmount
+          (event.target.value)} />
         </div>
         <div>
           <span>From:</span>
@@ -44,7 +63,7 @@ function Calculator(params) {
           <span>To:</span>
           <Select value={currencyTo} setCurrency={setCurrencyTo}/>
         </div>
-        <div>Result: {amount} </div>
+        <div>Result: {result} </div>
         <Button type="submit">Send</Button>
       </form>
     );

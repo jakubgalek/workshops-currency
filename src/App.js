2a54830264167f8d163376b7d78cba13430b699c
Kuba Gałek <xwondunx@gmail.com>
2020-12-23 12:27:42 +0100

    after routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router,
+Switch,
+Link,
+Route} from 'react-router-dom'
+
 //import { Button } from './components/Button';
 import Calculator from './pages/Calculator';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import './App.css';
 
 //const day='Saturday';
 
@@ -10,7 +17,22 @@ function App() {
   // JSX
   return (
     <div className="App">
-      <Calculator />
+      <Router>
+        <ul>
+          <li><Link to ="/">Home</Link></li>
+          <li><Link to ="/about">About</Link></li>
+          <li><Link to ="/contact">Contact</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/" exact={true}>
+            <Calculator />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact"component={Contact} />
+        </Switch>
+      </Router>
     </div>
   );
 }
